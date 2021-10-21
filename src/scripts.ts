7270bf883df4c6775b2e85a8870b795725a72388
GastonV <atis.sirmais@gmail.com>
2021-10-21 17:01:58 +0300

    added new lists from javascript

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,39 @@
-console.log('Ready for coding');
+const dropdown = document.querySelector(".js_dropdown");
+const btn = document.querySelector(".button__dropdown");
+const outsideScope = document.querySelector(".dropdown");
+
+const createDropdownButton: HTMLElement[] = [];
+const ahrefValues = [
+              "Kaķis", 
+              "Sašliks", 
+              "All above",
+];
+
+
+ahrefValues.forEach((listitems)=>{
+            let doc =    document.createElement("a");
+            doc.textContent = listitems;
+            doc.href = "#js";
+            doc.classList.add("non");
+            dropdown.appendChild(doc);
+});
+
+
+btn.addEventListener("click", () => {
+  dropdown.classList.toggle("show");
+  // if (dropdown.classList.contains("show")) {
+  //   dropdown.classList.remove("show");
+  // } else {
+  //   dropdown.classList.add("show");
+  // }
+});
+
+outsideScope.addEventListener("click", (event) => {
+  if (event.target !== btn && event.target !== dropdown) {
+    if (dropdown.classList.contains("show")) {
+      dropdown.classList.remove("show");
+
+    }
+  }
+});
+
